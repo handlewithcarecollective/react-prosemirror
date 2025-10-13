7656bcfd412dd92aa86f26a46345bba752586d6a
Shane Friedman <smoores-gpg@friedmans.us>
2025-10-13 14:50:06 -0400

    Force state updates and smarter OuterTag default

--- src/tiptap/TiptapEditorView.tsx
+++ src/tiptap/TiptapEditorView.tsx
@@ -4,6 +4,7 @@ import React, { ComponentType, ReactNode, useCallback } from "react";
 
 import { NodeViewComponentProps } from "../components/NodeViewComponentProps.js";
 import { ProseMirror } from "../components/ProseMirror.js";
+import { useForceUpdate } from "../hooks/useForceUpdate.js";
 
 interface Props {
   editor: Editor;
@@ -15,12 +16,17 @@ interface Props {
  * Render a Tiptap-compatible React ProseMirror editor.
  */
 export function TiptapEditorView({ editor, nodeViews, children }: Props) {
+  const forceUpdate = useForceUpdate();
   const dispatchTransaction = useCallback(
     (tr: Transaction) => {
       // @ts-expect-error calling private method
       editor.dispatchTransaction(tr);
+      // Tiptap's dispatchTransaction doesn't trigger
+      // a re-render, so we need to manually force
+      // one to ensure that React stays in sync.
+      forceUpdate();
     },
-    [editor]
+    [editor, forceUpdate]
   );
 
   const initialEditorProps = {
