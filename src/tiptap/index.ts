5ec626f8a9a4bf6e273fd29947653769192e5d87
Angela Guo <angela.guo@nytimes.com>
2026-01-12 07:59:20 -0800

    Allow client code to detect if a node view rendered with react-prosemirror (#174)
    * allow client code to detect if a node view is in RPM
    
    * version increment
    
    * Check EditorContext instead of NodeViewContext
    
    * remove duplicate import

--- src/tiptap/index.ts
+++ src/tiptap/index.ts
@@ -2,6 +2,7 @@ export { tiptapNodeView } from "./tiptapNodeView.js";
 export { TiptapEditorView } from "./TiptapEditorView.js";
 export { TiptapEditorContent } from "./TiptapEditorContent.js";
 export { useTiptapEditorEffect } from "./hooks/useTiptapEditorEffect.js";
+export { useIsInReactProseMirror } from "./hooks/useIsInReactProseMirror.js";
 export { useTiptapEditorEventCallback } from "./hooks/useTiptapEditorEventCallback.js";
 export { TiptapEditor } from "./TiptapEditor.js";
 export {
