3af085142e98cb6f6c064043fe5fd7bc08603a2e
Shane Friedman <smoores-gpg@friedmans.us>
2026-02-10 10:51:50 -0500

    Fix a bug in reorderSiblings command

--- src/tiptap/extensions/ReactProseMirror.ts
+++ src/tiptap/extensions/ReactProseMirror.ts
@@ -1,10 +1,7 @@
 import { Extension } from "@tiptap/core";
 
-import {
-  ReactKeysPluginMeta,
-  reactKeys,
-  reactKeysPluginKey,
-} from "../../plugins/reactKeys.js";
+import { reorderSiblingsOnTransaction } from "../../commands/reorderSiblings.js";
+import { reactKeys } from "../../plugins/reactKeys.js";
 
 declare module "@tiptap/core" {
   interface Commands<ReturnType> {
@@ -32,51 +29,14 @@ export const ReactProseMirror = Extension.create({
        *                and keep the other two in relative order, set `order` to `[1, 2, 0]`.
        */
       reorderSiblings(initialPos, order) {
-        return function reorderSiblingsCommand({ tr, dispatch }) {
-          const pos = tr.mapping.map(initialPos);
-          const $pos = tr.doc.resolve(pos);
-          if ($pos.start() !== pos) {
-            return false;
-          }
-
-          if (!dispatch) return true;
-
-          const nodes = $pos.parent.children;
-
-          const reordered = nodes
-            .map((node, i) => [node, i] as const)
-            .sort(([, a], [, b]) => order[a]! - order[b]!)
-            .map(([node]) => node);
-
-          tr.replaceWith(pos, $pos.parent.content.size + pos, reordered);
-
-          const meta: ReactKeysPluginMeta = { overrides: {} };
-
-          const oldPositions: number[] = [];
-          let start = pos;
-          for (const node of nodes) {
-            oldPositions.push(start);
-            start += node.nodeSize;
-          }
-
-          start = pos;
-
-          const newPositions: number[] = [];
-          for (let i = 0; i < reordered.length; i++) {
-            const node = reordered[i]!;
-            newPositions[order[i]!] = start;
-            start += node.nodeSize;
-          }
-
-          for (let i = 0; i < oldPositions.length; i++) {
-            const oldPosition = oldPositions[i]!;
-            const newPosition = newPositions[i]!;
-            meta.overrides[oldPosition] = newPosition;
-          }
-
-          tr.setMeta(reactKeysPluginKey, meta);
-
-          return true;
+        return function reorderSiblingsCommand({ tr, state, dispatch }) {
+          return reorderSiblingsOnTransaction(
+            initialPos,
+            order,
+            tr,
+            state,
+            dispatch
+          );
         };
       },
     };
