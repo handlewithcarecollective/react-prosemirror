42ae74b00e8812402f1b928acf98946e55c71733
Angela Guo <angelaa.guo@gmail.com>
2026-03-11 10:07:34 -0700

    give reactProseMirrorCommands a different extension name

--- src/tiptap/extensions/ReactProseMirrorCommands.ts
+++ src/tiptap/extensions/ReactProseMirrorCommands.ts
@@ -3,7 +3,7 @@ import { Extension, commands as coreCommands } from "@tiptap/core";
 import { updateAttributes } from "./commands/updateAttributes.js";
 
 export const ReactProseMirrorCommands = Extension.create({
-  name: "commands",
+  name: "reactProseMirrorCommands",
 
   addCommands() {
     return {
