941e0658848a340804cc1b828fbdb92cfde44e30
Angela Guo <angelaa.guo@gmail.com>
2026-05-12 10:17:46 -0700

    undo event callback hook c hanges

--- src/hooks/useEditorEventCallback.ts
+++ src/hooks/useEditorEventCallback.ts
@@ -2,10 +2,24 @@
 import type { EditorView } from "prosemirror-view";
 import { useCallback, useContext, useRef } from "react";
 
+import { AbstractEditorView } from "../AbstractEditorView.js";
 import { ReactEditorView } from "../ReactEditorView.js";
 import { EditorContext } from "../contexts/EditorContext.js";
 
 import { useEditorEffect } from "./useEditorEffect.js";
+
+function assertIsReactEditorView(
+  view: AbstractEditorView
+): asserts view is ReactEditorView {
+  if (view instanceof ReactEditorView) {
+    return;
+  }
+
+  throw new DOMException(
+    "ProseMirror document is not mounted",
+    "InvalidStateError"
+  );
+}
 
 /**
  * Returns a stable function reference to be used as an
@@ -14,7 +28,7 @@ import { useEditorEffect } from "./useEditorEffect.js";
  * The callback will be called with the EditorView instance
  * as its first argument.
  *
- * This hook will only run the callback in a component that is mounted
+ * This hook can only be used in a component that is mounted
  * as a child of the TiptapEditorView component, including
  * React node view components.
  */
@@ -30,10 +44,8 @@ export function useEditorEventCallback<This, T extends unknown[], R>(
 
   return useCallback(
     function (this: This, ...args: T) {
-      if (view instanceof ReactEditorView) {
-        return ref.current.call(this, view, ...args);
-      }
-      return;
+      assertIsReactEditorView(view);
+      return ref.current.call(this, view, ...args);
     },
     [view]
   );
