a354c3992fbfae34ca938942185fe5d1eb6dc822
Shane Friedman <smoores-gpg@friedmans.us>
2026-05-12 11:21:59 -0400

    Add a ref to widget component props

--- src/components/WidgetViewComponentProps.ts
+++ src/components/WidgetViewComponentProps.ts
@@ -1,8 +1,17 @@
-import { HTMLAttributes } from "react";
+import { AllHTMLAttributes, LegacyRef } from "react";
 
 import { ReactWidgetDecoration } from "../decorations/ReactWidgetType.js";
 
-export type WidgetViewComponentProps = {
+export type WidgetViewComponentProps<
+  Attributes = AllHTMLAttributes<HTMLElement>
+> = Attributes & {
   widget: ReactWidgetDecoration;
   getPos: () => number;
-} & HTMLAttributes<HTMLElement>;
+
+  // It's not really feasible to correctly type a Ref constraint,
+  // because it needs to be both covariant and contravariant (because
+  // it could be either a RefObject or a RefCallback). So we use any,
+  // here, instead of a more useful type like HTMLElement | null
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  ref: LegacyRef<any>;
+};
