96ca6883640c7590cda53302eaf020615c12e722
Angela Guo <angela.guo@nytimes.com>
2026-06-10 11:44:21 -0700

    Skip selectionToDOM while composing too (#243)

--- src/ReactEditorView.ts
+++ src/ReactEditorView.ts
@@ -297,14 +297,13 @@ export class ReactEditorView extends EditorView implements AbstractEditorView {
     const currentSelection = this.prevState.selection;
 
     const selectionChanged = !nextSelection.eq(currentSelection);
-
-    if (selectionChanged) {
+    if (selectionChanged && !this.composing) {
       super.update(this.nextProps);
     } else {
-      // If the selection hasn't changed between renders, force prosemirror-view to
-      // skip the selectionToDOM call. If a render happens after a DOM selection change
-      // but before the "selectionchange" event fired, calling selectionToDOM will cause
-      // the selection to be reset its the previous position.
+      // If the selection hasn't changed between renders or we're composing, force
+      // prosemirror-view to skip the selectionToDOM call. If a render happens after a DOM
+      // selection change but before the "selectionchange" event fired, calling
+      // selectionToDOM will cause the selection to be reset to its previous position.
       this.domObserver.setCurSelection();
       this.input.mouseDown = {
         allowDefault: false,
