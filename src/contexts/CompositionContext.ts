bb82980f079ea1886ede240ec79f576fa363deb2
Shane Friedman <smoores-gpg@friedmans.us>
2026-06-16 12:39:11 -0400

    Use useEditorStateSelector to only re-render relevant NodeView at composition boundaries

--- src/contexts/CompositionContext.ts
+++
@@ -1,9 +0,0 @@
-import { createContext } from "react";
-
-export interface CompositionContextValue {
-  freezeFrom: number | null;
-}
-
-export const CompositionContext = createContext<CompositionContextValue>({
-  freezeFrom: null,
-});
